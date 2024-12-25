f2ffe1a3aef93a6c1cc8ecc99945e1028fca4a67
Aziz0307 <Aziz03072018@gmail.com>
2024-12-25 16:40:42 +0500

    first commit

---
+++ Program.cs
@@ -0,0 +1,71 @@
+﻿using System;
+
+namespace ClassDZHday3
+{
+     class BitovayaTechinka
+    {
+        private string brend;
+        protected int moshnostUstroystva;
+        public int sostoyasnieUstroystva;
+
+        public string Brend
+        {
+            get { return brend; }
+            set { brend = value; }
+        }
+
+        public int MoshnostUstroystva
+        {
+            get { return moshnostUstroystva; }
+            set { moshnostUstroystva = value; }
+        }
+
+        public void Turn()
+        {
+            if (sostoyasnieUstroystva == 1)
+            {
+                Console.WriteLine("Ustroystva vklecheno.");
+            }
+            else if (sostoyasnieUstroystva == 0)
+            {
+                Console.WriteLine("Ustroystva viklecheno.");
+            }
+            else
+            {
+                Console.WriteLine("Sostoyasnie Ustroystva dolzhno = 0 ili 1.");
+            }
+        }
+    }
+    
+    class Kholodelnik : BitovayaTechinka
+    {
+        public Kholodelnik()
+        {
+            Brend = "Samsung";
+            moshnostUstroystva = 350;
+            sostoyasnieUstroystva = 1;
+        }
+    }
+
+    class StiralnayaMashina : BitovayaTechinka
+    {
+        public StiralnayaMashina()
+        {
+            Brend = "LG"; 
+            moshnostUstroystva = 250;
+            sostoyasnieUstroystva = 0;
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Kholodelnik kholodelnik = new Kholodelnik();
+            StiralnayaMashina stiralnayaMashina = new StiralnayaMashina();
+            Console.WriteLine($"Kholodelnik brend = {kholodelnik.Brend}     Moshnost = {kholodelnik.MoshnostUstroystva }");
+            kholodelnik.Turn();
+            Console.WriteLine($"Stiralnaya mashina brend = {stiralnayaMashina.Brend}     Moshnost = {stiralnayaMashina.MoshnostUstroystva }");
+            stiralnayaMashina.Turn();
+        }
+    }
+}
